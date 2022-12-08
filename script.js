88e6d00e740f70417d1ecd0f9ae2b7aac233997e
Hinalpatel8988 <118151523+Hinalpatel8988@users.noreply.github.com>
2022-12-08 22:20:19 +1100

    Code updated

--- script.js
+++ script.js
@@ -1,25 +1,73 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 var numbers = numbers
 var symbols = symbols
 var upperCaseLetters = upperCaseLetters
 var lowerCaseLetters = lowerCaseLetters
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-  function generatePassword();
-  const numbers = "0123456789"
-  const symbols = "!@#$%&*+"
-  const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz"
+  function generatePassword() {
+  const numbers = "0123456789";
+  const symbols = "!@#$%&*+";
+  const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+
+let includeUpperCaseLetters;
+let includelowerCaseLetters;
+let includeNumbers;
+let includeSymbols;
+
+let lenght = parseInt(
+  prompt(
+    "Please enter desired length (8-12 characters)!"
+  ));
+
+  if(!lenght) {
+    alert ("Number must be entered!")
+
+} else if (lenght < 8 || lenght > 12) {
+    lenght = parseInt(
+      prompt( "You must choose between 8 and 12 characters!")
+      );
+
+} else {
+  includeUpperCaseLetters = confirm("would you like lower case characters?");
+  includelowerCaseLetters = confirm("would you like upper case characters?");
+  includeNumbers = confirm("would you like numbers to include Number?");
+  includeSymbols = confirm("would you like numbers to include Symbols?");
+}
+  }
+
+if (
+  !includeUpperCaseLetters &&
+  !includelowerCaseLetters &&
+  !includeNumbers &&
+  !includeSymbols
+) {
+  alert("You must choose at least one type of characters!");
 }
 
-var lenght = 
+let characters = "";
+includeUpperCaseLetters ? (characters += upperCaseLetters) : "";
+includelowerCaseLetters ? (characters += lowerCaseLetters) : "";
+includeNumbers ? (characters += numbers) : "";
+includeSymbols ? (characters += symbols) : "";
 
-// Add event listener to generate button
+var password = "";
+for (var i = 0; i < length; i++) {
+  password += characters.charAt(
+    Math.floor(Math.random() * characters.length)
+  );  
+}
+return password;
+}
+
+
 generateBtn.addEventListener("click", writePassword);
