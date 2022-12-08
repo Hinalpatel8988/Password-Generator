331090e7449cbf9fffdda5cc1d61a19c778e867f
Hinalpatel8988 <118151523+Hinalpatel8988@users.noreply.github.com>
2022-12-08 18:18:37 +1100

    Partial Code added

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var numbers = numbers
+var symbols = symbols
+var upperCaseLetters = upperCaseLetters
+var lowerCaseLetters = lowerCaseLetters
 
 // Write password to the #password input
 function writePassword() {
@@ -8,7 +12,14 @@ function writePassword() {
 
   passwordText.value = password;
 
+  function generatePassword();
+  const numbers = "0123456789"
+  const symbols = "!@#$%&*+"
+  const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz"
 }
+
+var lenght = 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
