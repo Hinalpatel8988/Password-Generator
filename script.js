d4cd5e87b9e53f7551057d0d4c1b297780b2bf47
Hinalpatel8988 <118151523+Hinalpatel8988@users.noreply.github.com>
2022-12-11 14:19:36 +1100

    Final

--- script.js
+++ script.js
@@ -1,73 +1,69 @@
 // Assignment Code
 const generateBtn = document.querySelector("#generate");
-var numbers = numbers
-var symbols = symbols
-var upperCaseLetters = upperCaseLetters
-var lowerCaseLetters = lowerCaseLetters
-
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  let password = generatePassword();
   const passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
   function generatePassword() {
-  const numbers = "0123456789";
-  const symbols = "!@#$%&*+";
-  const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+  
+    const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+    const numbers = "0123456789";
+    const symbols = "!@#$%^&*_-+=";
 
-let includeUpperCaseLetters;
-let includelowerCaseLetters;
-let includeNumbers;
-let includeSymbols;
+    let includeUpperCaseLetters;
+    let includelowerCaseLetters;
+    let includeNumbers;
+    let includeSymbols;
 
-let lenght = parseInt(
-  prompt(
-    "Please enter desired length (8-12 characters)!"
-  ));
+    let length = parseInt(
+      prompt(
+        "Please enter desired length (8-12 characters)!"
+      )
+    );
 
-  if(!lenght) {
-    alert ("Number must be entered!")
+    if (!length) {
+      alert("Number must be entered!");
 
-} else if (lenght < 8 || lenght > 12) {
-    lenght = parseInt(
-      prompt( "You must choose between 8 and 12 characters!")
+    
+    } else if (length < 8 || length > 128) {
+      length = parseInt(
+        prompt("You must choose between 8 and 128 characters!")
       );
+   
+      includeUpperCaseLetters = confirm("Include Upper Case Letters?");
+      includelowerCaseLetters = confirm("Include Lower Case Letters?");
+      includeNumbers = confirm("Include Number?");
+      includeSymbols = confirm("Include Symbols?");
+    }
 
-} else {
-  includeUpperCaseLetters = confirm("would you like lower case characters?");
-  includelowerCaseLetters = confirm("would you like upper case characters?");
-  includeNumbers = confirm("would you like numbers to include Number?");
-  includeSymbols = confirm("would you like numbers to include Symbols?");
-}
+    if (
+      !includeUpperCaseLetters &&
+      !includelowerCaseLetters &&
+      !includeNumbers &&
+      !includeSymbols
+    ) {
+      alert("You must choose at least one type of characters!");
+    }
+
+    let characters = "";
+    includeUpperCaseLetters ? (characters += upperCaseLetters) : "";
+    includelowerCaseLetters ? (characters += lowerCaseLetters) : "";
+    includeNumbers ? (characters += numbers) : "";
+    includeSymbols ? (characters += symbols) : "";
+
+    let password = "";
+    for (let i = 0; i < length; i++) {
+      password += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
+    }
+    return password;
   }
-
-if (
-  !includeUpperCaseLetters &&
-  !includelowerCaseLetters &&
-  !includeNumbers &&
-  !includeSymbols
-) {
-  alert("You must choose at least one type of characters!");
 }
 
-let characters = "";
-includeUpperCaseLetters ? (characters += upperCaseLetters) : "";
-includelowerCaseLetters ? (characters += lowerCaseLetters) : "";
-includeNumbers ? (characters += numbers) : "";
-includeSymbols ? (characters += symbols) : "";
-
-var password = "";
-for (var i = 0; i < length; i++) {
-  password += characters.charAt(
-    Math.floor(Math.random() * characters.length)
-  );  
-}
-return password;
-}
-
-
+// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
